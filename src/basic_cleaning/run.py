1321cf2a937d0f25992bba1430f26f32dc4cdc07
HanByul Yang <hanbyul.yang@gmail.com>
2022-09-13 19:14:16 +0900

    Step 5: Pipeline Release and Updates
        - Train the model on a new data sample

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,10 @@ def go(args):
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
 
+    # Drop the outside of boundaries
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     logger.info("Convert last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
